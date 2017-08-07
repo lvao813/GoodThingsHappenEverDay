01079b07bbcb1eba56d7329d2ab63312c0acd81e
outsource <outsource@vloveapp.com>
2017-08-07 11:15:53 +0800

    添加tab选择器

--- GoodThingsHappenEverDay/main/code/History.js
+++ GoodThingsHappenEverDay/main/code/History.js
@@ -4,17 +4,26 @@ import {
   Text,
   View,
 } from 'react-native';
+ import ScrollableTabView, {DefaultTabBar,ScrollableTabBar} from 'react-native-scrollable-tab-view';
+ var Dimensions = require('Dimensions');
+var ScreenWidth = Dimensions.get('window').width;
+
  class History extends Component {
   
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome} >
-          This is History!
-        </Text>
-      
-      </View>
+      <ScrollableTabView
+                style={styles.container}
+                renderTabBar={() => <DefaultTabBar />}
+                tabBarUnderlineStyle={styles.lineStyle}
+                tabBarActiveTextColor='#FF0000'>
+
+                <Text style={styles.textStyle} tabLabel='Past Week'>No Entries</Text>
+                <Text style={styles.textStyle} tabLabel='Past Month'>No Entries</Text>
+                <Text style={styles.textStyle} tabLabel='Past Year'>No Entries</Text>
+                <Text style={styles.textStyle} tabLabel='All Time'>No Entries</Text>
+            </ScrollableTabView>
     );
   }
 }
@@ -37,5 +46,20 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  container: {
+         flex: 1,
+         marginTop: 20
+     },
+     lineStyle: {
+         width:ScreenWidth/4,
+         height: 2,
+         backgroundColor: '#FF0000',
+     },
+     textStyle: {
+         flex: 1,
+         fontSize:20,
+         marginTop:20,
+         textAlign:'center',
+     },
 });
 export default History
