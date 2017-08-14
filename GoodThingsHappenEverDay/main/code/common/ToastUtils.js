58334591b5b2a0d19e9468b653e3b10f4f043fd9
outsource <outsource@vloveapp.com>
2017-08-14 15:20:23 +0800

    为提示框添加另外两个样式，1.位置下移，2.持续时间更改，适应不同场景

--- GoodThingsHappenEverDay/main/code/common/ToastUtils.js
+++ GoodThingsHappenEverDay/main/code/common/ToastUtils.js
@@ -15,3 +15,35 @@ export const toastShort = (content) => {
     delay: 0
   });
 };
+
+export const toastLong = (content) => {
+  if (toast !== undefined) {
+    Toast.hide(toast);
+  }
+  toast = Toast.show(content.toString(), {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.CENTER,
+    shadow: true,
+    animation: true,
+    hideOnPress: true,
+    delay: 0
+  });
+};
+
+export const toastShortBy = (content) => {
+  if (toast !== undefined) {
+    Toast.hide(toast);
+  }
+  toast = Toast.show(content.toString(), {
+    duration: Toast.durations.SHORT,
+    position: Toast.positions.CENTER,
+    shadow: true,
+    animation: true,
+    hideOnPress: false,
+    delay: 0,
+   
+  });
+  setTimeout(function () {
+    Toast.hide(toast);
+}, 700);
+};
