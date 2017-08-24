59a9715047935989fd7fdd22a9722219a857df80
outsource <outsource@vloveapp.com>
2017-08-24 18:16:47 +0800

    数据联动

--- GoodThingsHappenEverDay/main/code/History.js
+++ GoodThingsHappenEverDay/main/code/History.js
@@ -5,6 +5,7 @@ import {
   View,
   ScrollView,
   Image,
+  FlatList,
 } from 'react-native';
 import { getItem,saveItem} from './common/AsyncStorage'
  import ScrollableTabView, {DefaultTabBar,ScrollableTabBar} from 'react-native-scrollable-tab-view';
@@ -22,6 +23,7 @@ var ScreenWidth = Dimensions.get('window').width;
         WeekArry:[],
         MonthArry:[],
         YearArry:[],
+        AllArry:[],
       
     };
     
@@ -39,19 +41,39 @@ var ScreenWidth = Dimensions.get('window').width;
       // alert(this.state.keyarry)
       let keyarry = this.state.keyarry; 
       for(let i in this.state.keyarry){
+        var promise = getItem(keyarry[i]).then((result) => {
+          this.state.AllArry.push(result)
+            
+        }).catch((error) => {
+          
+        })
         if(keyarry[i]>Weektime&&keyarry[i]<timestamp){
-            this.state.WeekArry.push(keyarry[i])
+          var promise = getItem(keyarry[i]).then((result) => {
+            this.state.WeekArry.push(result)
+              
+          }).catch((error) => {
+            
+          })
+            // this.state.WeekArry.push()
             // alert(this.state.WeekArry)
           // alert(keyarry[1])
         }
         if(keyarry[i]>Monthtime&&keyarry[i]<timestamp){
-            this.state.MonthArry.push(keyarry[i])
-            // alert(this.state.MonthArry)
+          var promise = getItem(keyarry[i]).then((result) => {
+            this.state.MonthArry.push(result)
+              
+          }).catch((error) => {
+            
+          })
         }
         if(keyarry[i]>Yeartime&&keyarry[i]<timestamp){
-                this.state.YearArry.push(keyarry[i])
-                // alert(this.state.YearArry)
-              }
+          var promise = getItem(keyarry[i]).then((result) => {
+            this.state.YearArry.push(result)
+              
+          }).catch((error) => {
+            
+          })
+        }
       }
       
       
@@ -69,33 +91,93 @@ var ScreenWidth = Dimensions.get('window').width;
     // }, 1000);
     // // alert(this.state.WeekArry)
   }
+  _separator = () => {
+    return <View style={{height:2,}}/>;
+  }
+  _renderItemWeek = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,10)
+    return ( 
+              <View style={styles.card}>
+                <Text >{thtime}</Text>
+                <Text style={{fontSize:20,color:'#13227a',fontWeight:'bold'}}>{journltext}</Text>
+              </View>
+      )
+  }
+  _renderItemMonth = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,10)
+    return ( 
+              <View style={styles.card}>
+                <Text >{thtime}</Text>
+                <Text style={{fontSize:20,color:'#13227a',fontWeight:'bold'}}>{journltext}</Text>
+              </View>
+      )
+  }
+  _renderItemYear = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,10)
+    return ( 
+              <View style={styles.card}>
+                <Text >{thtime}</Text>
+                <Text style={{fontSize:20,color:'#13227a',fontWeight:'bold'}}>{journltext}</Text>
+              </View>
+      )
+  }
+  _renderItemAll = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,10)
+    return ( 
+              <View style={styles.card}>
+                <Text >{thtime}</Text>
+                <Text style={{fontSize:20,color:'#13227a',fontWeight:'bold'}}>{journltext}</Text>
+              </View>
+      )
+  }
   render() {
     return (
-      <ScrollView>
-            <View style={{flexDirection:'row',backgroundColor:'#fff',borderColor:'#666',borderBottomWidth:1}}>
-                <View style={styles.topView}>
-                      <Text style={styles.topText}>History</Text>
-                      
-                </View>
-                <Image style={{height:40,width:40,alignItems:'center',justifyContent:'center'}} 
-                      source={require('./image/hot.png')}
-                      ><Text style={{textAlign:'center',fontWeight:'bold',fontSize:20}}
-                >1</Text></Image>
-            </View>
+     
+            
             <ScrollableTabView
-                  // style={styles.container}
+                  style={{marginTop: 20, }}
                   renderTabBar={() => <DefaultTabBar />}
                   tabBarUnderlineStyle={styles.lineStyle}
                   tabBarActiveTextColor='#FF0000'>
-
-                  <View style={{height:90,width:100,backgroundColor:'#000'}} tabLabel='Past Week'> 
-                      <View style={{height:30,width:50,backgroundColor:'#666',marginTop:50}}></View>
-                  </View>
-                  <View style={styles.container} tabLabel='Past Month'></View>
-                  <View style={styles.container} tabLabel='Past Year'></View>
-                  <View style={styles.container} tabLabel='All Time'></View>
+                  <ScrollView tabLabel="Past Week" style={styles.tabView}>
+                      <FlatList
+                    
+                      ItemSeparatorComponent={this._separator}
+                      renderItem={this._renderItemWeek}
+                      data={this.state.WeekArry}>
+                      </FlatList>
+                </ScrollView>
+                <ScrollView tabLabel="Past Month" style={styles.tabView}>
+                    <FlatList
+                    
+                      ItemSeparatorComponent={this._separator}
+                      renderItem={this._renderItemMonth}
+                      data={this.state.MonthArry}>
+                      </FlatList>
+                </ScrollView>
+                <ScrollView tabLabel="Past Year" style={styles.tabView}>
+                      <FlatList
+                      
+                        ItemSeparatorComponent={this._separator}
+                        renderItem={this._renderItemYear}
+                        data={this.state.YearArry}>
+                        </FlatList>
+              </ScrollView>
+              <ScrollView tabLabel="All Time" style={styles.tabView}>
+                      <FlatList
+                      
+                        ItemSeparatorComponent={this._separator}
+                        renderItem={this._renderItemAll}
+                        data={this.state.AllArry}>
+                        </FlatList>
+              </ScrollView>
+                  
             </ScrollableTabView>
-      </ScrollView>
+      
     );
   }
 }
@@ -147,6 +229,24 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     color:'#000',
     paddingLeft:20,
+  },
+  tabView: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.01)',
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 100,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 2, height: 2, },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    borderRadius:10,
   },
 });
 export default History
